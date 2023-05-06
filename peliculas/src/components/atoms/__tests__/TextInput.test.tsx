ac2e1478608d1f4db5ecba057abeace90672ac81
Luis Cascante <luisalonzocascante@gmail.com>
2023-05-06 09:22:48 -0600

    jest config

---
+++ peliculas/src/components/atoms/__tests__/TextInput.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { TextInput } from "../TextInput";
+
+describe("atoms/TextInput", () => {
+  it("should render correctly", () => {
+    // Arrange
+    render(<TextInput />);
+  });
+});
